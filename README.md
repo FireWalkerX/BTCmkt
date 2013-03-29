127d77788eebda7a5a5573ce64f54553d449d399
Razican <admin@razican.com>
2013-03-29 15:28:55 +0100

    Added CodeIgniter engine. In commit 5269c1cf2d5ae749309b06194f0a2a737a9ff114

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-BTCmkt
-======
-
 Bitcoin Market
+==============
+
+This is intended to be a bitcoin market, to be used for trading Bitcoins, Euros, and eventually more forex.
---
+++ market/controllers/main.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Main extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+}
+
+
+/* End of file main.php */
+/* Location: ./application/controllers/main.php */
